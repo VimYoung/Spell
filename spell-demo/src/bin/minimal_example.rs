77191b6c8e6c55e4d1718f2b21492e51fa80279f
VimYoung <bhaumik2386@gmail.com>
2026-06-08 09:30:52 +0530

    Fix: Temporary error fix and minimal example addition in spell-demo

---
+++ spell-demo/src/bin/minimal_example.rs
@@ -0,0 +1,31 @@
+use std::{env, error::Error};
+
+use slint::ComponentHandle;
+use spell_framework::{
+    cast_spell,
+    layer_properties::{LayerAnchor, LayerType, WindowConf},
+};
+slint::include_modules!();
+spell_framework::generate_widgets![AppWindow];
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let window_conf = WindowConf::builder()
+        .width(376_u32)
+        .height(576_u32)
+        .anchor_1(LayerAnchor::TOP)
+        .anchor_2(LayerAnchor::LEFT)
+        .margins(5, 0, 0, 10)
+        .layer_type(LayerType::Top)
+        .build()
+        .unwrap();
+
+    let ui = AppWindowSpell::invoke_spell("counter-widget", window_conf);
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+    cast_spell!(ui)
+}
